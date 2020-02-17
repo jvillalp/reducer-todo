5a3c5c514e404d09bb51c7eb0931fd3694a81eb6
Juana Villalpando <juana.v14@gmail.com>
2020-02-17 11:52:37 -0800

    add reducer.js

---
+++ src/reducers/reducer.js
@@ -0,0 +1,18 @@
+
+export const initialState = {
+    todos: [
+        {
+            item: 'Finish Project',
+            completed: false,
+            id: 0
+        },
+        {
+            item: 'Eat fav snack',
+            completed: false,
+            id: 1
+        }
+    ]
+}
+export const reducter = () =>{
+
+}
